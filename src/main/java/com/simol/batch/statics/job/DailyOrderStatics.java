d9fffd1677168b7e4152b3b8393f482993cd39ea
juno-choi <ililil9482@naver.com>
2023-12-20 17:10:33 +0900

    [feat] 상품 특정 기간내로 불러오도록 추가
    <상세>
    
    날짜는 파라미터로 받아서 처리하는게 나중에 에러가 났을 경우에도 처리하기 좋을 듯

--- src/main/java/com/simol/batch/statics/job/DailyOrderStatics.java
+++ src/main/java/com/simol/batch/statics/job/DailyOrderStatics.java
@@ -15,6 +15,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 
@@ -40,8 +41,16 @@ public class DailyOrderStatics {
 
     public Tasklet staticsTasklet() {
         return (contribution, chunkContext) -> {
+
             log.info("statics job 실행...");
-            List<OrderEntity> all = orderEntityRepository.findAll();
+            LocalDateTime now = LocalDateTime.of(2023, 12, 1, 0, 0, 0);
+            int year = now.getYear();
+            int month = now.getMonthValue();
+            int day = now.getDayOfMonth();
+
+            LocalDateTime startDateTime = LocalDateTime.of(year, month, day, 0, 0, 0);
+            LocalDateTime endDateTime = LocalDateTime.of(year, month, day, 23, 59, 59);
+            List<OrderEntity> all = orderEntityRepository.findByCreatedAtBetween(startDateTime, endDateTime);
             //todo 옵션 리스트도 불러와야 함
 
             //todo 옵션 1개를 상품 1개로 봐야 됨
--- src/main/java/com/simol/batch/statics/repository/OrderEntityRepository.java
+++ src/main/java/com/simol/batch/statics/repository/OrderEntityRepository.java
@@ -4,6 +4,10 @@ import com.simol.batch.statics.domain.entity.OrderEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
 public interface OrderEntityRepository extends JpaRepository<OrderEntity, Long> {
+    List<OrderEntity> findByCreatedAtBetween(LocalDateTime startDateTime, LocalDateTime endDateTime);
 }
