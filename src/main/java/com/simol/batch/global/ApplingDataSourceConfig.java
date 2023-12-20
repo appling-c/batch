c5c66aa521e3ff3d9a1f4c8d8b9e356fea145c55
juno-choi <ililil9482@naver.com>
2023-12-20 15:19:19 +0900

    [feat] jpa entity 설정 추가

--- src/main/java/com/simol/batch/global/ApplingDataSourceConfig.java
+++ src/main/java/com/simol/batch/global/ApplingDataSourceConfig.java
@@ -1,16 +1,30 @@
 package com.simol.batch.global;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.hibernate.boot.model.naming.PhysicalNamingStrategyStandardImpl;
+import org.hibernate.jpa.HibernatePersistenceProvider;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.orm.jpa.hibernate.SpringImplicitNamingStrategy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
+import java.util.Objects;
+import java.util.Properties;
 
 @Configuration
+@EnableJpaRepositories(
+        basePackages = "com.simol.batch.statics",
+        entityManagerFactoryRef = "applingEntityManager",
+        transactionManagerRef = "applingTransactionManager"
+)
 public class ApplingDataSourceConfig {
     @Value("${spring.datasource.hikari.appling.jdbc-url}")
     private String url;
@@ -25,22 +39,36 @@ public class ApplingDataSourceConfig {
     private String driverClassName;
 
     @Bean
+    @Qualifier("applingDataSource")
     public DataSource applingDateSource() {
-        DataSourceBuilder<?> dataSourceBuilder = DataSourceBuilder.create();
-        dataSourceBuilder.driverClassName(driverClassName);
-        dataSourceBuilder.url(url);
-        dataSourceBuilder.username(username);
-        dataSourceBuilder.password(password);
-        return dataSourceBuilder.build();
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setJdbcUrl(url);
+        hikariConfig.setUsername(username);
+        hikariConfig.setPassword(password);
+        hikariConfig.setDriverClassName(driverClassName);
+        return new HikariDataSource(hikariConfig);
     }
 
     @Bean
-    public PlatformTransactionManager applingTransactionManagerApp() {
-        return new DataSourceTransactionManager(applingDateSource());
+    @Qualifier("applingEntityManager")
+    public LocalContainerEntityManagerFactoryBean applingEntityManager() {
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(applingDateSource());
+        em.setPackagesToScan("com.simol.batch.statics");
+        em.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        em.setPersistenceProvider(new HibernatePersistenceProvider());
+
+        Properties properties = new Properties();
+        // 물리적인 네이밍 전략 설정
+        properties.setProperty("hibernate.physical_naming_strategy", "com.simol.batch.global.CustomPhysicalNamingStrategy");
+
+        em.setJpaProperties(properties);
+        return em;
     }
 
     @Bean
-    public JdbcTemplate applingJdbcTemplate() {
-        return new JdbcTemplate(applingDateSource());
+    @Qualifier("applingTransactionManager")
+    public PlatformTransactionManager applingTransactionManager() {
+        return new JpaTransactionManager(Objects.requireNonNull(applingEntityManager().getObject()));
     }
 }
--- src/main/java/com/simol/batch/global/BatchDataSourceConfig.java
+++ src/main/java/com/simol/batch/global/BatchDataSourceConfig.java
@@ -1,14 +1,23 @@
 package com.simol.batch.global;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import jakarta.persistence.EntityManagerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
+import java.util.Objects;
 
 @Configuration
 public class BatchDataSourceConfig {
@@ -23,6 +32,7 @@ public class BatchDataSourceConfig {
 
     @Value("${spring.datasource.hikari.driver-class-name}")
     private String driverClassName;
+
 
     @Bean
     @Primary
